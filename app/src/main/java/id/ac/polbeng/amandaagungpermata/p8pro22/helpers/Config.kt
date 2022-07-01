eede7ae83bba6e92aee16a0351ad4141d098182b
Gina <ginasahiba@gmail.com>
2022-07-01 17:39:50 +0700

    jasa api online

---
+++ app/src/main/java/id/ac/polbeng/amandaagungpermata/p8pro22/helpers/Config.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.amandaagungpermata.p8pro22.helpers
+
+class Config {
+    companion object {
+        const val SPLASH_TIME_OUT:Long = 3000
+        const val BASE_URL = "http://10.0.2.2:4000/"
+        const val EXTRA_FRAGMENT_ID = "FRAGMENT_ID"
+    }
+}
